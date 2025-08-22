fcba8c8d5fb688c39fe7175ed7149809a274f2b4
Melina Bustos <melina.m.bustos@gmail.com>
2025-08-22 11:58:55 -0300

    ejercicio 4 finalizado

--- Ejercicio 4 TP1/Ejercicio 4 TP1/Program.cs
+++ Ejercicio 4 TP1/Ejercicio 4 TP1/Program.cs
@@ -10,6 +10,13 @@ namespace Ejercicio_4_TP1
     {
         static void Main(string[] args)
         {
+            String lado;
+
+            Console.WriteLine("Ingresa el lado del cuadrado :");
+            lado = Console.ReadLine();
+            int ladoDelCuadrado = int.Parse(lado);
+            Console.WriteLine("El perimetro del cuadrado es : {0}", ladoDelCuadrado*ladoDelCuadrado);
+            Console.ReadKey();
         }
     }
 }
