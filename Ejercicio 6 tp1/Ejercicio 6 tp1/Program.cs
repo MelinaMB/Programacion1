2714144a61d06c8d19364ac0c485485a6ab81b5d
Melina Bustos <melina.m.bustos@gmail.com>
2025-08-22 12:40:28 -0300

    ejercicio 6 tp1

---
+++ Ejercicio 6 tp1/Ejercicio 6 tp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6_tp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String numero;
+
+            Console.WriteLine("Ingresa un numero :");
+            numero = Console.ReadLine();
+            int numeroParaMulticplicar = int.Parse(numero);
+
+            for (int i = 0; i <=10; i++) {
+
+                Console.WriteLine("La tabla de multiplicar de este numero es : {0} ", numeroParaMulticplicar * i);
+
+            }
+        }
+    }
+}
