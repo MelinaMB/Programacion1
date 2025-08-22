c82b84c61d814a4085c4ec4a9f919893b683b07c
Melina Bustos <melina.m.bustos@gmail.com>
2025-08-22 12:27:04 -0300

    ejercicio 5 tp 1

---
+++ Ejercicio 5 tp1/Ejercicio 5 tp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5_tp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String base1;
+            String altura;
+
+            Console.WriteLine("Ingresa el base del rectangulo :");
+            base1 = Console.ReadLine();
+            int base1Rectangulo = int.Parse(base1);
+            Console.WriteLine("Ingresa el altura del rectangulo :");
+            altura = Console.ReadLine();
+            int alturaRectangulo = int.Parse(altura);
+            Console.WriteLine("La superficie del rectangulo es : {0} ",( 2* (base1Rectangulo + alturaRectangulo)));
+            Console.ReadKey();
+        }
+    }
+}
