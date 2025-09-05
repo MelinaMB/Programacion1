90039b4b4b8a97b1a0da62427fc1f3c47be7d409
Melina Bustos <melina.m.bustos@gmail.com>
2025-09-04 21:42:11 -0300

    clase2

--- Ejemplo/Ejemplo/Form1.Designer.cs
+++ Ejemplo/Ejemplo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ejemplo
 {
-    partial class Form1
+    partial class frmFormularioInicial
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.Label();
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Formulario 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 213);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.AutoSize = true;
+            this.txtNombre.Location = new System.Drawing.Point(120, 197);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(94, 13);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.Text = "Ingresa su nombre";
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(139, 261);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(75, 23);
+            this.btnSaludar.TabIndex = 3;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(88, 301);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(245, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(70, 343);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(288, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // frmFormularioInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnSaludar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "frmFormularioInicial";
+            this.Text = " Form1";
+            this.Load += new System.EventHandler(this.frmFormularioInicial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtNombre;
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Ejemplo/Ejemplo/Form1.cs
+++ Ejemplo/Ejemplo/Form1.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace Ejemplo
 {
-    public partial class Form1 : Form
+    public partial class frmFormularioInicial : Form
     {
-        public Form1()
+        public frmFormularioInicial() //constructor
         {
             InitializeComponent();
+        }
+
+        private void frmFormularioInicial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmForm2 frm = new frmForm2();
+            frm.BackColor = Color.White;
+            frm.Show();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor, ingrese su nombre");
+
+            }
+            else
+            {
+                MessageBox.Show("Bienvenido" + textBox1.Text);
+
+            }
+        }
+
+       
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            progressBar1.Value = (int)numericUpDown1.Value;
         }
     }
 }
