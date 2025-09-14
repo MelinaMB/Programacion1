75e9513192704cab19e7e4e0d026216d510436d2
Melina Bustos <melina.m.bustos@gmail.com>
2025-09-14 11:44:43 -0300

    Ejemplo de recursividad

---
+++ Recursividad/Recursividad/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    internal class Program
+    {
+        static int Factorial(int n) { 
+            
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int res = Factorial(4);
+        }
+    }
+}
