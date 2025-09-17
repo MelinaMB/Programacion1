4ad15935403dd9f97d598aaca83e06dd381a807a
Melina Bustos <melina.m.bustos@gmail.com>
2025-09-16 23:48:48 -0300

    Ejemplo pilas primera parte

---
+++ Pilas/Pilas/MiPila.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilas
+{
+    internal class MiPila
+    {
+        MiNodo tope;
+
+        public MiNodo Tope() { 
+            return tope;
+        }
+        void Apilar(MiNodo unNodo) {
+
+            if (tope == null)
+            {
+
+                tope = unNodo;
+            }
+            else { 
+                MiNodo aux = tope;
+                tope = unNodo;
+                tope.Siguiente = aux;
+            }
+        
+        }
+
+        void Desapilar() {
+            if (tope != null) { 
+
+                tope = tope.Siguiente;
+            }
+            
+        }
+    }
+}
