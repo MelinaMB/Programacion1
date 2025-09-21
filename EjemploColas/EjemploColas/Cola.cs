0da6f3de25f4000f00d7c00c5e3802e748cb8282
Melina Bustos <melina.m.bustos@gmail.com>
2025-09-20 21:47:19 -0300

    ejemplo de colas

---
+++ EjemploColas/EjemploColas/Cola.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploColas
+{
+    internal class Cola
+    {
+        Nodo _inicio;
+        public void Encolar(Nodo unNodo) { // agraga una persona al final
+
+        }
+
+        public void Desencolar() { // quita la primera persona
+
+            _inicio = _inicio.Siguiente;
+        }
+
+        public Nodo Inicio { // persona al inicio de la cola
+
+            get { return _inicio; }
+
+        }
+    }
+}
