e380dd2316df01e2c575adb7e7503772d3e9edf1
Melina Bustos <melina.m.bustos@gmail.com>
2025-12-07 22:16:03 -0300

    Tp4 recursividad-listas

---
+++ TP4Recursividad-Listas/TP4Recursividad-Listas/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Recursividad
+Implementación de un método recursivo que reciba un parámetro de tipo entero y luego llame en
+forma recursiva con el valor del parámetro menos 1. Deberá mostrar por consola : “10 9 8 7 6 5 4
+3 2 1 “
+Listas
+Se tiene una lista de pacientes de un hospital y se desea realizar un sistema sobre registro de
+estos.
+Los datos de los pacientes son código, nombres, apellido, dirección y teléfono.
+El sistema debe permitir:
+a.Registrar un nuevo paciente
+b. Agregar después del seleccionado.
+c. Mostrar Listado
+*/
+namespace TP4Recursividad_Listas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TP4Recursividad-Listas/TP4Recursividad-Listas/Nodo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4Recursividad_Listas
+{
+    internal class Nodo
+    {
+    }
+}
